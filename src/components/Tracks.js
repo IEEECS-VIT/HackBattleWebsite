a64d322c1985dc45e1b0ed3e58afc45debf4f98d
Likhit Ajeesh <likhitajeesh20@gmail.com>
2023-09-17 15:57:10 +0530

    fix: track svg

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -30,7 +30,7 @@ const Tracks = () => {
         >
           <path
             className={inViewport2 ? 'track-path' : 'track-no-path'}
-            d='M407.5 1H412.5H417.5M407.5 1H398M407.5 1V53L375 98H217.5V762H296M398 1H389V52L369.5 80H1V218M398 1V52L372.5 88.5L207.5 89.5V522.5H1V637M417.5 1H426.5M417.5 1V53L446.5 92.5H628.5V273H562M426.5 1H436V52L456 77H824.5V218M426.5 1V52L452.5 85H637V522.5H824.5V637'
+            d='M407.5 1H412.5H417.5M407.5 1H398M407.5 1V53L375 98H217.5V550H296M398 1H389V52L369.5 80H1V218M398 1H426.5M417.5 1V53L446.5 92.5H628.5V273H562M426.5 1H436V52L456 77H824.5V218M426.5'
             stroke='#FF009E'
             strokeWidth='1.5'
           />
@@ -38,26 +38,13 @@ const Tracks = () => {
       </div>
 
       <div className='tracks_div'>
-        <TracksCard
-          name='WEB 3.0'
-          
-        />
-        <TracksCard
-          name='HEALTHCARE '
-          
-        />
-        <TracksCard
-          name='VIT CENTRIC '
-         
-        />
-        <TracksCard
-          name='OPEN INNOVATION (SDGs)'
-         
-        />
+        <TracksCard name='WEB 3.0' />
+        <TracksCard name='HEALTHCARE ' />
+        <TracksCard name='VIT CENTRIC ' />
+        <TracksCard name='OPEN INNOVATION (SDGs)' />
       </div>
     </div>
   );
 };
 
 export default Tracks;
-
