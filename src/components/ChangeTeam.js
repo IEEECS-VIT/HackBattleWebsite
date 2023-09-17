a810c4b1f800ecef5e3b5d0a19a21cf285623c88
SanyaRaj08 <rajsanya05@gmail.com>
2023-09-17 15:31:35 +0530

    updated

--- src/components/ChangeTeam.js
+++ src/components/ChangeTeam.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Axios from 'axios';
+import { Config } from './Config';
 
 const ChangeTeam = () => {
   const [activeLink, setActiveLink] = useState('home');
