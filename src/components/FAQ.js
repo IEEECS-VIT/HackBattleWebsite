21f83735b4d410457516b44b01bb3a590cb96cf0
Likhit Ajeesh <likhitajeesh20@gmail.com>
2023-09-17 22:37:09 +0530

    fix: faq fontsize

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -22,26 +22,25 @@ const FAQ = () => {
           answer='While we recommend teams of at least three members, individuals can join our Discord server to find collaboration opportunities and form teams within our community.'
         />
         <FAQCard
-          question="What are the available tracks or categories for the hackathon?"
+          question='What are the available tracks for the hackathon?'
           answer=' We offer a variety of tracks or categories to cater to different interests and skills. Participants can choose from options such as AI/ML, IoT, Web Development, and more.'
         />
         <FAQCard
-          question="How much is the total prize pool for the hackathon?"
+          question='How much is the total prize pool for the hackathon?'
           answer='The total prize pool for the hackathon is impressive, with prizes exceeding 1 lakh INR for winning teams. Exact prize details will be announced closer to the event date.'
         />
         <FAQCard
-          question="What should I bring to the hackathon?"
+          question='What should I bring to the hackathon?'
           answer='Essential items such as laptops, chargers, ID-cards should be carried.'
         />
         <FAQCard
-          question="Can I work on a pre-existing project?"
+          question='Can I work on a pre-existing project?'
           answer='Submission of a new project created based on the provided problem statements is expected. Submissions of pre existing projects would lead to disqualification of the participants from the hackathon.'
         />
         <FAQCard
-          question="How can I contact the organizers during the hackathon?"
+          question='How can I contact the organizers during the hackathon?'
           answer='Volunteers shall be available throughout the hackathon in the venue itself for any help required. Participants can also reach out on the discord channel.'
         />
-        
       </div>
     </div>
   );
